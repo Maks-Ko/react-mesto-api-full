b370bb6ddeca88a0f592e3dccdcececd154b021b
Maksim Kozhevnikov <kozhevnikow@yandex.ru>
2021-12-14 17:27:25 +0500

    fix: cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,12 +9,11 @@ module.exports = (req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    return res.end();
   }
 
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
+  const requestHeaders = req.headers;
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
