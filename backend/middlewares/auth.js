b0debc78849349b3b5e1f78af2e8bbca19caf4ba
Maksim Kozhevnikov <kozhevnikow@yandex.ru>
2021-12-14 09:46:18 +0500

    fix: обработать все ошибки централизованно через обработчик

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -8,7 +8,6 @@ module.exports = (req, res, next) => {
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new UnauthorizedError('Необходима авторизация');
-    //return res.status(401).send({ message: 'Необходима авторизация' });
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -19,7 +18,6 @@ module.exports = (req, res, next) => {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
-    //return res.status(401).send({ message: 'Необходима авторизация' });
   }
 
   req.user = payload;
