c18ca2b8a4bbff5363987d0230d690b66480d5b7
Maksim Kozhevnikov <kozhevnikow@yandex.ru>
2021-12-14 10:07:54 +0500

    fix: тип поля likes

--- backend/models/card.js
+++ backend/models/card.js
@@ -24,9 +24,9 @@ const cardSchema = new mongoose.Schema({
     ref: user,
   },
   likes: {
-    type: mongoose.Schema.Types.ObjectId,
-    default: [],
+    type: mongoose.Schema.Types.Array,
     ref: user,
+    default: [],
   },
   createdAt: {
     type: Date,
